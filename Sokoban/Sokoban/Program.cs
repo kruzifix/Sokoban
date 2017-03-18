bb0372b7dbf3f43c327a20bddc2c8e7ef4b82c1e
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 19:18:44 +0100

    cleaned up Program.cs

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -3,19 +3,15 @@
 namespace Sokoban
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new SokobanApp())
+            {
                 game.Run();
+            }
         }
     }
 #endif
