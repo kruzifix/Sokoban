bd9e2b63ade6687a9cb66c4d45cbb9d70b85f428
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 21:41:31 +0100

    added TiledMap.cs

---
+++ Sokoban/SokobanContentPipeline/TiledMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace SokobanContentPipeline
+{
+    [Serializable]
+    [XmlRoot("map")]
+    public class TiledMap
+    {
+        [XmlAttribute("width")]
+        public int Width { get; set; }
+    }
+}
