be03382c36aef0d64ac0fe2402f8feba097940f5
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 23:44:00 +0100

    added TiledLayer.cs

---
+++ Sokoban/SokobanGame/TiledLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokobanGame
+{
+    public class TiledLayer
+    {
+        public string Name { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public int[,] Data { get; private set; }
+
+        public TiledLayer(string name, int width, int height)
+        {
+            Name = name;
+            Width = width;
+            Height = height;
+
+            Data = new int[width, height];
+        }
+
+        public void SetData(int i, int value)
+        {
+            SetData(i % Width, i / Width, value);
+        }
+
+        public void SetData(int x, int y, int value)
+        {
+            Data[x, y] = value;
+        }
+    }
+}
--- Sokoban/SokobanGame/TiledMap.cs
+++ Sokoban/SokobanGame/TiledMap.cs
@@ -1,12 +1,35 @@
-﻿namespace SokobanGame
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SokobanGame
 {
     public class TiledMap
     {
         public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int TileWidth { get; private set; }
+        public int TileHeight { get; private set; }
+        
+        private Texture2D tilesetTexture;
 
-        public TiledMap(int width)
+        public Texture2D TilesetTexture { get { return tilesetTexture; } }
+
+        private List<TiledLayer> layers;
+        
+        public TiledMap(int width, int height, int tileWidth, int tileHeight, Texture2D tilesetTexture)
         {
             Width = width;
+            Height = height;
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+
+            this.tilesetTexture = tilesetTexture;
+            layers = new List<TiledLayer>();
+        }
+
+        public void AddLayer(TiledLayer layer)
+        {
+            layers.Add(layer);
         }
     }
 }
