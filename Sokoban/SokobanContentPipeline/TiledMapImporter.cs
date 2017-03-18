280196d5daee787ba6e00c4b700b7db825d866b5
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 21:41:20 +0100

    added TiledMapImporter.cs

---
+++ Sokoban/SokobanContentPipeline/TiledMapImporter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace SokobanContentPipeline
+{
+    [ContentImporter(".tmx", DefaultProcessor = "TiledMapProcessor", DisplayName = "TiledMap Importer")]
+    public class TiledMapImporter : ContentImporter<TiledMap>
+    {
+        public override TiledMap Import(string filename, ContentImporterContext context)
+        {
+            context.Logger.LogMessage("Importing XML file: {0}", filename);
+
+            using (var streamReader = new StreamReader(filename))
+            {
+                var deserializer = new XmlSerializer(typeof(TiledMap));
+                return (TiledMap)deserializer.Deserialize(streamReader);
+            }
+        }
+    }
+}
