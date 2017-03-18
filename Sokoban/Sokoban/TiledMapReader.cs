2b7d26ca215e1e08f999c20c5ea6bc0fa2b3f941
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 23:59:18 +0100

    Tiled map basic content reading working

--- Sokoban/Sokoban/TiledMapReader.cs
+++ Sokoban/Sokoban/TiledMapReader.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using SokobanGame;
 
 namespace Sokoban
@@ -8,8 +9,32 @@ namespace Sokoban
         protected override TiledMap Read(ContentReader input, TiledMap existingInstance)
         {
             int width = input.ReadInt32();
+            int height = input.ReadInt32();
+            int tileWidth = input.ReadInt32();
+            int tileHeight = input.ReadInt32();
 
-            return new TiledMap(width);
+            string tileSetPath = input.ReadString();
+            Texture2D tileSetTexture = input.ContentManager.Load<Texture2D>(tileSetPath);
+            
+            var map = new TiledMap(width, height, tileWidth, tileHeight, tileSetTexture);
+
+            int layerCount = input.ReadInt32();
+
+            for (int i = 0; i < layerCount; i++)
+            {
+                string name = input.ReadString();
+                int layerWidth = input.ReadInt32();
+                int layerHeight = input.ReadInt32();
+                var layer = new TiledLayer(map, name, layerWidth, layerHeight);
+
+                for (int j = 0; j < layerWidth * layerHeight; j++)
+                {
+                    layer.SetData(j, input.ReadInt32());
+                }
+                map.AddLayer(layer);
+            }
+
+            return map;
         }
     }
 }
