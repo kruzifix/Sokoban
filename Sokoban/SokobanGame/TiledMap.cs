759a641cfacf3a74e239c1014184574f490e6656
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-18 22:38:38 +0100

    added SokobanGame class library

---
+++ Sokoban/SokobanGame/TiledMap.cs
@@ -0,0 +1,12 @@
+﻿namespace SokobanGame
+{
+    public class TiledMap
+    {
+        public int Width { get; private set; }
+
+        public TiledMap(int width)
+        {
+            Width = width;
+        }
+    }
+}
