2c695755f139088b32f1cf66fc12b23330eb9c2a
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-19 12:09:37 +0100

    added Singleton construct to ScreenManager

--- Sokoban/SokobanGame/Screen/ScreenManager.cs
+++ Sokoban/SokobanGame/Screen/ScreenManager.cs
@@ -1,6 +1,27 @@
-﻿namespace SokobanGame.Screen
+﻿using Microsoft.Xna.Framework;
+
+namespace SokobanGame.Screen
 {
     public class ScreenManager
     {
+        private static ScreenManager instance = null;
+
+        public static ScreenManager CreateScreenManager(Game game, GameScreen startupScreen)
+        {
+            if (instance != null)
+            {
+                instance = new ScreenManager(game, startupScreen);
+            }
+            return instance;
+        }
+
+        public static ScreenManager Instance { get { return instance; } }
+
+        private Game game;
+
+        private ScreenManager(Game game, GameScreen startupScreen)
+        {
+            this.game = game;
+        }
     }
 }
