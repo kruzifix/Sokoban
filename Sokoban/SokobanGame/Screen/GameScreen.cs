f26aa4f8dfff448814e0b07adcc7ab91f2afb05d
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-19 12:14:28 +0100

    added fundamental members to gamescreen

--- Sokoban/SokobanGame/Screen/GameScreen.cs
+++ Sokoban/SokobanGame/Screen/GameScreen.cs
@@ -1,6 +1,19 @@
-﻿namespace SokobanGame.Screen
+﻿using Microsoft.Xna.Framework;
+
+namespace SokobanGame.Screen
 {
     public abstract class GameScreen
     {
+        public bool BlocksDraw { get; private set; }
+        public bool BlocksUpdate { get; private set; }
+
+        public GameScreen(bool blocksDraw, bool blocksUpdate)
+        {
+            BlocksDraw = blocksDraw;
+            BlocksUpdate = blocksUpdate;
+        }
+
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
     }
 }
