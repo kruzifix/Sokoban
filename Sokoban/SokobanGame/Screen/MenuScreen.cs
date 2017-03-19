4f73cf2d0300ac097f49208d192be6e6ae594292
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-19 12:40:55 +0100

    added MenuScreen

---
+++ Sokoban/SokobanGame/Screen/MenuScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace SokobanGame.Screen
+{
+    public class MenuScreen : Screen
+    {
+        public MenuScreen()
+            : base(true, true)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
