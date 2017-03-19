e017ce4a2eda0560aa7f25a49a019f33c4b0c1cb
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-19 12:48:05 +0100

    added debug mesage to MenuScreen

--- Sokoban/SokobanGame/Screen/MenuScreen.cs
+++ Sokoban/SokobanGame/Screen/MenuScreen.cs
@@ -14,6 +14,7 @@ namespace SokobanGame.Screen
         public override void Draw(GameTime gameTime)
         {
             SokobanGame.Instance.DrawDebugMessage("MenuScreen", new Vector2(10, 10), Color.Black);
+            SokobanGame.Instance.DrawDebugMessage(string.Format("GameTime: {0}", gameTime.TotalGameTime), new Vector2(10, 30), Color.Black);
         }
 
         public override void Update(GameTime gameTime)
