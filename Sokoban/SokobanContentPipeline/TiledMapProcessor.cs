e4b4f790fc621ccdf9b72330ab973af9150a1b10
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-20 21:30:14 +0100

    added room processing

--- Sokoban/SokobanContentPipeline/TiledMapProcessor.cs
+++ Sokoban/SokobanContentPipeline/TiledMapProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using SokobanContentPipeline.Output;
+using SokobanGame;
 
 /*
  * Reference: http://dylanwilson.net/creating-custom-content-importers-for-the-monogame-pipeline
@@ -43,20 +45,66 @@ namespace SokobanContentPipeline
                     Height = input.Height,
                     TileWidth = input.TileWidth,
                     TileHeight = input.TileHeight,
-                    Tileset = tileset
+                    Tileset = tileset,
+                    Room = new RoomProcessorOutput(input.Width, input.Height)
                 };
+                output.Room.InitialState = new RoomStateProcessorOutput();
                 
                 foreach (var layerData in input.Layers)
                 {
                     context.Logger.LogMessage("Parsing Layer: {0}", layerData.Name);
                     var layer = new TiledMapLayerOutput(layerData.Name, layerData.Width, layerData.Height);
+
                     string lines = layerData.Data.Content.Replace(Environment.NewLine, "");
                     string[] tokens = lines.Split(',');
                     for (int i = 0; i < tokens.Length; i++)
                     {
                         layer[i] = int.Parse(tokens[i].Trim());
                     }
-                    output.Layers.Add(layer);
+
+                    if (layer.Name.Equals(WallLayerName))
+                    {
+                        context.Logger.LogMessage("Copying Walls to Room.");
+                        // copy walls
+                        for (int i = 0; i < output.Width; i++)
+                        {
+                            for (int j = 0; j < output.Height; j++)
+                            {
+                                if (layer.Data[i, j] > 0)
+                                    output.Room.Walls[i, j] = 1;
+                            }
+                        }
+                        output.Layers.Add(layer);
+                    }
+                    else if (layer.Name.Equals(ObjectLayerName))
+                    {
+                        context.Logger.LogMessage("Parsing initial room state.");
+
+                        List<IntVec> switches = new List<IntVec>();
+                        List<IntVec> boxes = new List<IntVec>();
+                        for (int i = 0; i < output.Width; i++)
+                        {
+                            for (int j = 0; j < output.Height; j++)
+                            {
+                                if (layer.Data[i, j] - 1 == PlayerId)
+                                    output.Room.InitialState.PlayerPosition = new IntVec(i, j);
+                                else if (layer.Data[i, j] - 1 == SwitchId)
+                                    switches.Add(new IntVec(i, j));
+                                else if (layer.Data[i, j] - 1 == BoxId)
+                                    boxes.Add(new IntVec(i, j));
+                            }
+                        }
+                        output.Room.InitialState.Switches = switches.ToArray();
+                        output.Room.InitialState.Boxes = boxes.ToArray();
+
+                        context.Logger.LogMessage("\tPlayerPosition: {0}", output.Room.InitialState.PlayerPosition);
+                        context.Logger.LogMessage("\tSwitches: {0}", output.Room.InitialState.Switches.Length);
+                        context.Logger.LogMessage("\tBoxes: {0}", output.Room.InitialState.Boxes.Length);
+                    }
+                    else
+                    {
+                        output.Layers.Add(layer);
+                    }
                 }
                 return output;
             }
