12a9106f1009ab0cbbd2c6fb3103f168758ac3f4
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-20 21:38:42 +0100

    added global Assets class

---
+++ Sokoban/SokobanGame/Assets.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SokobanGame.Tiled;
+
+namespace SokobanGame
+{
+    public static class Assets
+    {
+        public static SpriteFont DebugFont { get; private set; }
+        public static TiledMap TestMap { get; private set; }
+
+        public static void LoadAssets(ContentManager content)
+        {
+            DebugFont = content.Load<SpriteFont>("debug_font");
+            TestMap = content.Load<TiledMap>("sokoban");
+        }
+    }
+}
