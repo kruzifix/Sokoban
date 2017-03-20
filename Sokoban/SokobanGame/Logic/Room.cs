c42d79ded56086d99d6d565d3b379e603828afe0
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-20 20:15:26 +0100

    added RoomState and Room

---
+++ Sokoban/SokobanGame/Logic/Room.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace SokobanGame.Logic
+{
+    public class Room
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int[,] walls;
+
+        private RoomState initialState;
+        public RoomState CurrentState { get; private set; }
+        private Stack<RoomState> history;
+
+        public Room(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            walls = new int[Width, Height];
+        }
+
+        public void SetWall(int x, int y, int v)
+        {
+            walls[x, y] = v;
+        }
+
+        public void Reset()
+        {
+            history.Clear();
+            CurrentState = initialState.Copy();
+        }
+
+        public void Undo()
+        {
+            if (history.Count > 0)
+                CurrentState = history.Pop();
+        }
+    }
+}
---
+++ Sokoban/SokobanGame/Logic/RoomState.cs
@@ -0,0 +1,43 @@
+﻿namespace SokobanGame.Logic
+{
+    public class RoomState
+    {
+        public IntVec PlayerPosition { get; private set; }
+        public IntVec[] Switches { get; private set; }
+        public IntVec[] Boxes { get; private set; }
+
+        public RoomState(IntVec playerPosition, IntVec[] switches, IntVec[] boxes)
+        {
+            PlayerPosition = playerPosition;
+            Switches = switches;
+            Boxes = boxes;
+        }
+
+        public RoomState Copy()
+        {
+            return new RoomState(PlayerPosition, (IntVec[])Switches.Clone(), (IntVec[])Boxes.Clone());
+        }
+
+        public bool IsSolved()
+        {
+            for (int i = 0; i < Switches.Length; i++)
+            {
+                bool boxFound = false;
+
+                for (int j = 0; j < Boxes.Length; j++)
+                {
+                    if (Switches[i] == Boxes[j])
+                    {
+                        boxFound = true;
+                        break;
+                    }
+                }
+
+                if (!boxFound)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
