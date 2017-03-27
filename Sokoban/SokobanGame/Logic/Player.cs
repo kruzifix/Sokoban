af113dd70714d10bca70fda2e8b21e7c08214350
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-27 20:47:23 +0200

    added Player

---
+++ Sokoban/SokobanGame/Logic/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace SokobanGame.Logic
+{
+    public class Player : Entity
+    {
+        public override Entity Copy()
+        {
+            return new Player() { Pos = this.Pos };
+        }
+    }
+}
