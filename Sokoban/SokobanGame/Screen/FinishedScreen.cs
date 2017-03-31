3c2a71016fd817a8b3101621bdf2fb311d4aafad
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-03-31 20:03:07 +0200

    added FinishedScreen

---
+++ Sokoban/SokobanGame/Screen/FinishedScreen.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SokobanGame.Screen
+{
+    public class FinishedScreen : Screen
+    {
+        private int finishedLevel;
+        private int moveCount;
+
+        public FinishedScreen(int finishedLevel, int moveCount) 
+            : base(false, true)
+        {
+            this.finishedLevel = finishedLevel;
+            this.moveCount = moveCount;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
