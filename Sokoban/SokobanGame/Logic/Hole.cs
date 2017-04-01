be0916992a4e1d03d90550fc3260a863de3d74e3
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-04-01 23:31:36 +0200

    added Hole entity

---
+++ Sokoban/SokobanGame/Logic/Hole.cs
@@ -0,0 +1,12 @@
+﻿namespace SokobanGame.Logic
+{
+    public class Hole : Entity
+    {
+        public bool Filled { get; set; } = false;
+
+        public override Entity Copy()
+        {
+            return new Hole() { Pos = Pos };
+        }
+    }
+}
