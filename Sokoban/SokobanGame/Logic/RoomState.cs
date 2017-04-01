0d4e5a1b148e51d5abf7bddda3e74c1ea9fff38d
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-04-01 23:39:01 +0200

    refactored Roomstate entity filtering

--- Sokoban/SokobanGame/Logic/RoomState.cs
+++ Sokoban/SokobanGame/Logic/RoomState.cs
@@ -8,43 +8,18 @@ namespace SokobanGame.Logic
         public IntVec PlayerPosition { get; set; }
         public List<Entity> Entities { get; private set; }
 
-        private List<Box> boxes;
-        public List<Box> Boxes
-        {
-            get
-            {
-                if (boxes == null)
-                    boxes = Entities.Where(e => e is Box).Cast<Box>().ToList();
-                return boxes;
-            }
-        }
-
-        private List<StickyBox> stickyBoxes;
-        public List<StickyBox> StickyBoxes
-        {
-            get
-            {
-                if (stickyBoxes == null)
-                    stickyBoxes = Entities.Where(e => e is StickyBox).Cast<StickyBox>().ToList();
-                return stickyBoxes;
-            }
-        }
-
-        private List<Hole> holes;
-        public List<Hole> Holes
-        {
-            get
-            {
-                if (holes == null)
-                    holes = Entities.Where(e => e is Hole).Cast<Hole>().ToList();
-                return holes;
-            }
-        }
+        public List<Box> Boxes { get; private set; }
+        public List<StickyBox> StickyBoxes { get; private set; }
+        public List<Hole> Holes { get; private set; }
 
         public RoomState(IntVec playerPosition, IEnumerable<Entity> ents)
         {
             PlayerPosition = playerPosition;
             Entities = new List<Entity>(ents);
+
+            Boxes = Entities.Where(e => e is Box).Cast<Box>().ToList();
+            StickyBoxes = Entities.Where(e => e is StickyBox).Cast<StickyBox>().ToList();
+            Holes = Entities.Where(e => e is Hole).Cast<Hole>().ToList();
         }
 
         public RoomState Copy()
