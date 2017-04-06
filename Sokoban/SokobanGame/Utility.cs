a5f7ce2053f131c066e00a7ed6667d5dd7f9c52a
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-04-06 13:22:50 +0200

    added Utility.cs

---
+++ Sokoban/SokobanGame/Utility.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SokobanGame
+{
+    public static class Utility
+    {
+        public static void DrawRect(this SpriteBatch sb, float x, float y, float width, float height, Color color)
+        {
+            DrawRect(sb, (int)x, (int)y, (int)width, (int)height, color);
+        }
+
+        public static void DrawRect(this SpriteBatch sb, int x, int y, int width, int height, Color color)
+        {
+            DrawRect(sb, new Rectangle(x, y, width, height), color);
+        }
+
+        public static void DrawRect(this SpriteBatch sb, Rectangle rect, Color color)
+        {
+            sb.Begin();
+            sb.Draw(Assets.PixelTexture, rect, color);
+            sb.End();
+        }
+    }
+}
