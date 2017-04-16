6d2dc6aea280fd0555e8cda8d5b4dc3147a978d5
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-04-16 20:27:19 +0200

    added gamepad buttons

--- Sokoban/SokobanGame/Assets.cs
+++ Sokoban/SokobanGame/Assets.cs
@@ -25,6 +25,13 @@ namespace SokobanGame
         public static Rectangle SrcZ { get; private set; }
         public static Rectangle SrcEnter { get; private set; }
 
+        public static Texture2D PadBtns { get; private set; }
+        public static Rectangle SrcDPad { get; private set; }
+        public static Rectangle SrcA { get; private set; }
+        public static Rectangle SrcB { get; private set; }
+        public static Rectangle SrcX { get; private set; }
+        public static Rectangle SrcY { get; private set; }
+
         public static TiledMap[] Levels { get; private set; }
 
         public static void LoadAssets(ContentManager content)
@@ -45,6 +52,13 @@ namespace SokobanGame
             SrcR = new Rectangle(500, 0, 100, 100);
             SrcZ = new Rectangle(600, 0, 100, 100);
             SrcEnter = new Rectangle(700, 0, 100, 100);
+
+            PadBtns = content.Load<Texture2D>("pad_buttons");
+            SrcDPad = new Rectangle(0, 0, 100, 100);
+            SrcA = new Rectangle(100, 0, 100, 100);
+            SrcB = new Rectangle(200, 0, 100, 100);
+            SrcX = new Rectangle(300, 0, 100, 100);
+            SrcY = new Rectangle(400, 0, 100, 100);
 
             string levelsPath = "Levels";
             DirectoryInfo dir = new DirectoryInfo(content.RootDirectory + "/" + levelsPath);
