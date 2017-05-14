e11e9a6cc689119d2ac8e3a7c18cb89bd541272c
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2017-05-14 17:52:19 +0200

    added text to creditsscreen

--- Sokoban/SokobanGame/Screen/CreditsScreen.cs
+++ Sokoban/SokobanGame/Screen/CreditsScreen.cs
@@ -5,20 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using SokobanGame.Input;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SokobanGame.Screen
 {
     public class CreditsScreen : Screen
     {
+        SpriteFont titleFont;
+        SpriteFont font;
+
         public CreditsScreen()
             : base(true, true)
-        { }
+        {
+            titleFont = Assets.TitleFont;
+            font = Assets.TextFont;
+        }
 
         public override void Draw(GameTime gameTime)
         {
             SokobanGame.Instance.GraphicsDevice.Clear(Colors.MenuScreenBackground);
 
+            int width = SokobanGame.Width;
+            int height = SokobanGame.Height;
 
+            float hw = width * 0.5f;
+
+            sb.Begin();
+            sb.DrawString(titleFont, "Credits", new Vector2(hw, 120), Color.White, Align.Center);
+
+            sb.DrawString(font, "Programming & Design", new Vector2(hw, 300), Color.White, Align.Center);
+            sb.DrawString(font, "David Cukrowicz", new Vector2(hw, 350), Color.Black, Align.Center);
+
+            sb.DrawString(font, "Assets", new Vector2(hw, 420), Color.White, Align.Center);
+            sb.DrawString(font, "Kenney.nl", new Vector2(hw, 470), Color.Black, Align.Center);
+
+            sb.DrawString(Assets.DebugFont, "This project was made as part of the bachelor's program 'MultiMediaTechnology' of the University of Applied Sciences Salzburg.",
+                new Vector2(hw, height - 100), Color.Black, Align.Center);
+
+            sb.End();
         }
 
         public override void Update(GameTime gameTime)
